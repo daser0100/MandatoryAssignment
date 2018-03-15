112ebc7b28b9bf4f6deb789cf03b5d4e557fa026
daser0100 <34938564+daser0100@users.noreply.github.com>
2018-03-15 12:05:28 +0100

    Add files via upload

--- code/index.php
+++ code/index.php
@@ -1,3 +1,27 @@
 <?php
+echo'<!DOCTYPE html>
+<html>
+	<head>
+		<title>1362</title>
+		<link rel="icon" href="elements/kitchen.ico">
+		<link href="style.css" type="text/css" rel="stylesheet">
+		<link href="elements/menu/style.css" type="text/css" rel="stylesheet">
+	</head>
+	<body>
+		<div class="page">
+			<div class="sideleft"></div>
+			<div class="center">
+				<div class="banner"><p>banner</p></div>';
+				
+				include 'elements/menu/index.html';
 
-?>
+echo'			<div id="page">';
+					
+				include 'elements/pages/AboutMe/index.html';	
+				
+echo'			</div>
+			</div>
+			<div class="sideright"></div>
+		</div>
+	</body>
+</html>';
